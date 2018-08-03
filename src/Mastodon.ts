263bdea957f099465b838bc78a94c71e4da034df
Genbu Hase <hasegenbu@gmail.com>
2018-08-03 18:50:53 +0900

    Mastodonクラス追加 * getメソッド追加

---
+++ src/Mastodon.ts
@@ -0,0 +1,22 @@
+/**
+ * Mastodonインスタンスとの通信を行うクラス
+ */
+class Mastodon {
+	public token: string;
+
+	public constructor (public instance: string) {
+		this.token = UserProperties.getProperty(instance);
+	}
+
+	public get (apiUrl: string, params: Array<[ string, string ]> = []): GoogleAppsScript.URL_Fetch.HTTPResponse {
+		const option: object = {
+			method: "GET",
+			headers: { Authorization: `Bearer ${this.token}` }
+		};
+
+		const paramStrs: Array<string> = [];
+		for (const param of params) paramStrs.push(`${param[0]}=${param[1]}`);
+
+		return UrlFetchApp.fetch(`https://${this.instance}/${apiUrl}${params.length ? `?${paramStrs.join("&")}`: ""}`);
+	}
+}
