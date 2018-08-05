3f28f2bb0a7a7554818d18242c57274fef865eea
Genbu Hase <hasegenbu@gmail.com>
2018-08-05 09:37:52 +0900

    数種の変更 * Array<any> -> any[]に変更 * getメソッドの引数変更 * for-of文からforEach文へ変更

--- src/Mastodon.ts
+++ src/Mastodon.ts
@@ -4,7 +4,7 @@
 interface TootOptions {
 	status: string;
 	in_reply_to_id?: number;
-	media_ids?: Array<number>;
+	media_ids?: number[];
 	sensitive?: boolean;
 	spoiler_text?: string;
 	visibility?: "public" | "unlisted" | "private" | "direct" | string;
@@ -18,7 +18,7 @@ interface TootOptions {
  * @author Genbu Hase
  */
 export class Mastodon {
-	/** @param instance Mastodonインスタンスのドメイン(ex: itabashi.0j0.jp) */
+	/** @param instance Mastodonインスタンスのドメイン (ex: itabashi.0j0.jp) */
 	public constructor (public instance: string) {
 		this.token = UserProperties.getProperty(instance);
 	}
@@ -26,7 +26,13 @@ export class Mastodon {
 	/** インスタンスと紐付けられたトークン */
 	public token: string;
 
-	public get (apiUrl: string, params: Array<[ string, string ]> = []): GoogleAppsScript.URL_Fetch.HTTPResponse {
+	/**
+	 * application/json形式でGET通信を行います
+	 * 
+	 * @param apiUrl エンドポイントのURL
+	 * @param params クエリパラメータ (ex: { id: 1, "exclude_types[]": [ "reblog", "favourite" ] })
+	 */
+	public get (apiUrl: string, params: object = {}): GoogleAppsScript.URL_Fetch.HTTPResponse {
 		const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
 			method: "get",
 			headers: {
@@ -35,12 +41,27 @@ export class Mastodon {
 			}
 		};
 
-		const paramStrs: Array<string> = [];
-		for (const param of params) paramStrs.push(`${param[0]}=${param[1]}`);
+		const paramStrs: string[] = [];
+		for (const name in params) {
+			const param: any = params[name];
 
-		return UrlFetchApp.fetch(`https://${this.instance}/${apiUrl}${params.length ? `?${paramStrs.join("&")}`: ""}`, options);
+			if (Array.isArray(param)) {
+				param.forEach(prop => paramStrs.push(`${name}=${prop}`));
+				return;
+			}
+
+			paramStrs.push(`${name}=${param}`);
+		}
+
+		return UrlFetchApp.fetch(`https://${this.instance}/${apiUrl}${paramStrs.length ? `?${paramStrs.join("&")}`: ""}`, options);
 	}
 
+	/**
+	 * application/json形式でPOST通信を行います
+	 * 
+	 * @param apiUrl エンドポイントのURL
+	 * @param payload 送信するデータ
+	 */
 	public post (apiUrl: string, payload: object = {}): GoogleAppsScript.URL_Fetch.HTTPResponse {
 		const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
 			method: "post",
@@ -65,9 +86,9 @@ export class Mastodon {
  * @author Genbu Hase
  */
 export class MoEClient extends Mastodon {
+	public static readonly SubjectMatcher = /MoE(:[^@<>]+(?=@))?@([^<>]*)(?:<(.+)>)?/;
+
 	public static readonly MagicMatcher = {
-		Subject: /MoE(:[^@<>]+(?=@))?@([^<>]*)(?:<(.+)>)?/,
-		
 		CW: /\[CW ?\| ?(.+)\]\r?\n/i,
 		Emoji: /\[(:[^:]+:) ?\| ?([^\]]+)\]/
 	};
@@ -91,10 +112,10 @@ export class MoEClient extends Mastodon {
 		text = text.replace(cw[0], "");
 
 		const emojis: RegExpMatchArray = text.match(new RegExp(MoEClient.MagicMatcher.Emoji.source, "g")) || [];
-		for (const emojiStr of emojis) {
+		emojis.forEach(emojiStr => {
 			const emoji: RegExpMatchArray = emojiStr.match(MoEClient.MagicMatcher.Emoji);
 			text = text.replace(emoji[0], `${emoji[1]} `.repeat(parseInt(emoji[2], 10)));
-		}
+		});
 
 		const options: TootOptions = {
 			status: [
